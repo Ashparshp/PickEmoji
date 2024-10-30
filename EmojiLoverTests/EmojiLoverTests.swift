61d630531f34d25337688a9086c089685f4f08c8
Ashparshp <ashparsh.connects@gmail.com>
2024-10-31 03:43:38 +0530

    Initial Commit

---
+++ EmojiLoverTests/EmojiLoverTests.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverTests.swift
+//  EmojiLoverTests
+//
+//  Created by Ashparsh Pandey on 31/10/24.
+//
+
+import Testing
+@testable import EmojiLover
+
+struct EmojiLoverTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
